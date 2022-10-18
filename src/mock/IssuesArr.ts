faa623ec984b062452f15f8d6ce3cc5dbf2ce6c2
Vladimir Talan <vovkus@gmail.com>
2022-10-18 21:24:22 +0300

    fork: add mock def octopus arr

---
+++ src/mock/IssuesArr.ts
@@ -0,0 +1,25 @@
+import { Issue } from '../types';
+
+export const MockIssuesArr: Issue[] = [
+  {
+    id: 0,
+    title: 'First Title',
+    url: 'First Url',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id: 1,
+    title: 'Second Title',
+    url: 'Second Url',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+  {
+    id: 2,
+    title: 'Third Title',
+    url: 'Third Url',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+];
