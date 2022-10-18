b5f7064deafdff0b16e794f1d73539a1139fd35c
Vladimir Talan <vovkus@gmail.com>
2022-10-18 21:18:27 +0300

    fork: update def organisation, repo refactor: update styles

--- src/components/Home/Header/Header.tsx
+++ src/components/Home/Header/Header.tsx
@@ -1,38 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import { useDispatch, useSelector } from 'react-redux';
-
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { RootState } from '../../../store/store';
-import { setItems } from '../../../store/reducers/items';
-
 export const Header = () => {
-  const dispatch = useDispatch();
-  const { items: items } = useSelector((state: RootState) => state.items);
-
-  const handlePress = () => {
-    dispatch(setItems(items + 1));
-  };
-
   return (
     <View style={styles.container}>
-      <Text style={styles.header} onPress={handlePress}>
-        Search Github Issues: {items}
-      </Text>
+      <Text style={styles.header}>Search Github Issues</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#FFAB00',
+    backgroundColor: '#FF8000',
   },
+
   header: {
     fontSize: 35,
     textAlign: 'center',
-    borderColor: Colors.black,
+    borderColor: '#000000',
     color: '#FFFFFF',
-    borderBottomWidth: 3,
+    borderBottomWidth: 2,
+    borderTopWidth: 2,
   },
 });
