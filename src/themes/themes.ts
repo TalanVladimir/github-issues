e9a66ecda3118c558fd6a0b68573037ae787dc30
Vladimir Talan <vovkus@gmail.com>
2022-10-20 21:43:07 +0300

    fork: add Themes

---
+++ src/themes/themes.ts
@@ -0,0 +1,14 @@
+export const Theme = {
+  dark: false,
+  colors: {
+    primaryColor: '#7cb342',
+    primaryLightColor: '#aee571',
+    primaryDarkColor: '#4b830d',
+    primaryTextColor: '#000000',
+
+    secondaryColor: '#311b92',
+    secondaryLightColor: '#6746c3',
+    secondaryDarkColor: '#000063',
+    secondaryTextColor: '#ffffff',
+  },
+};
