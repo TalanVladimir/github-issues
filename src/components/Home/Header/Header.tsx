47b35ba93892d3a9a34ef38f15b301cd419b5c7b
Vladimir Talan <vovkus@gmail.com>
2022-10-20 21:43:43 +0300

    fork: add Themes

--- src/components/Home/Header/Header.tsx
+++ src/components/Home/Header/Header.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, useColorScheme, View } from 'react-native';
+import { Theme } from '../../../themes/themes';
 
 export const Header = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Search Github Issues</Text>
+    <View
+      style={[
+        styles.container,
+        {
+          backgroundColor: isDarkMode
+            ? Theme.colors.primaryDarkColor
+            : Theme.colors.secondaryDarkColor,
+        },
+      ]}
+    >
+      <Text
+        style={[
+          styles.header,
+          {
+            color: isDarkMode
+              ? Theme.colors.primaryColor
+              : Theme.colors.secondaryColor,
+          },
+        ]}
+      >
+        Search Github Issues{' '}
+      </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#FF8000',
-  },
+  container: {},
 
   header: {
     fontSize: 35,
     textAlign: 'center',
     borderColor: '#000000',
-    color: '#FFFFFF',
     borderBottomWidth: 2,
     borderTopWidth: 2,
   },
